dc98572790ba047f20bf448f4e89f5f8076aeea7
BT_1N3_11 <sample@bths.edu>
2023-09-21 08:51:15 -0400

    first item user orders

--- src/RihamTipCalculator.java
+++ src/RihamTipCalculator.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class RihamTipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the Tip Calculator! What is the first item you wish to order today?");
+        System.out.println("Welcome to the Everything Resturant!");
+        System.out.println("I am your Tip Calculator for today!");
         System.out.println("How many people do we have dining on this fine day?");
         int groupAmount = scan.nextInt();
         System.out.println("Please enter the tip percentage for today. (0-100)");
         double tipPercent = scan.nextDouble();
         System.out.println("We have our special menu with fixed prices for meals, or you can just choose your own order.");
+        System.out.println("So, what's the first item you wish to order?");
+        String userFirstItem = scan.nextLine();
     }
 }
