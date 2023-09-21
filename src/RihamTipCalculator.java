79c9837f8950728f6c528d9a6d82cf33a8e42006
BT_1N3_11 <sample@bths.edu>
2023-09-21 09:27:08 -0400

    WHILE CODE FIXED YAY

--- src/RihamTipCalculator.java
+++ src/RihamTipCalculator.java
@@ -12,13 +12,13 @@ public class RihamTipCalculator {
         System.out.println("We have our special menu with fixed prices for meals, or you can just choose your own order.");
         double userInput = 0;
         double totalCost = 0;
-        String userAns = "";
-        while (!userAns.equals("no")) {
+        int userAns = 0;
+        while (userAns != 1) {
             System.out.println("What is the item you wish to order? Please type in dollars and cents, eg. 12.50 (-1 if you finish ordering).");
             userInput = scan.nextDouble();
             totalCost += userInput;
-            System.out.println("Do you wish to order more? Type no if not.");
-            userAns = scan.nextLine();
+            System.out.println("Do you wish to order? Type 1 if not.");
+            userAns = scan.nextInt();
         }
         System.out.println("test totalCost: " + totalCost);
     }
