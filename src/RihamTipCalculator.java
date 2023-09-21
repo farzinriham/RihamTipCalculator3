f61ee981535ab788d696dafd79e769cf0213a4cb
BT_1N3_11 <sample@bths.edu>
2023-09-21 08:42:41 -0400

    tip percentage

--- src/RihamTipCalculator.java
+++ src/RihamTipCalculator.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class RihamTipCalculator {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the Tip Calculator! What is the first item you wish to order today?");
         System.out.println("How many people do we have dining on this fine day?");
-        int groupAmount = x.nextInt();
+        int groupAmount = scan.nextInt();
+        System.out.println("Please enter the tip percentage for today. (0-100)");
+        double tipPercent = scan.nextDouble();
         System.out.println("We have our special menu with fixed prices for meals, or you can just choose your own order.");
     }
 }
