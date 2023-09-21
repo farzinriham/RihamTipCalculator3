26ec62c2a75a3646214a454bfa408a649f801cd3
BT_1N3_11 <sample@bths.edu>
2023-09-21 09:03:33 -0400

    whiel code added

--- src/RihamTipCalculator.java
+++ src/RihamTipCalculator.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class RihamTipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the Everything Resturant!");
+        System.out.println("Welcome to the Everything Restaurant!");
         System.out.println("I am your Tip Calculator for today!");
         System.out.println("How many people do we have dining on this fine day?");
         int groupAmount = scan.nextInt();
         System.out.println("Please enter the tip percentage for today. (0-100)");
         double tipPercent = scan.nextDouble();
         System.out.println("We have our special menu with fixed prices for meals, or you can just choose your own order.");
-        System.out.println("So, what's the first item you wish to order?");
-        String userFirstItem = scan.nextLine();
+        double userInput = 0;
+        double totalCost = 0;
+        String userAns = "";
+        while (userAns = "none") {
+            System.out.println("What is the item you wish to order? Please type in dollars and cents, eg. 12.50 (-1 if you finish ordering).");
+            userInput = scan.nextDouble();
+            totalCost += userInput;
+        }
+        System.out.println("test totalCost: " + totalCost);
     }
 }
