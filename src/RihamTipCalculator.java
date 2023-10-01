ac49bed3c7f71b287ee3bd98083b9db2d8a34a9a
Farzin Riham <friham07@gmail.com>
2023-09-30 21:29:30 -0400

    lists updated 9/30/23 9:29 pm

--- src/RihamTipCalculator.java
+++ src/RihamTipCalculator.java
@@ -13,28 +13,45 @@ import java.util.Scanner;
 public class RihamTipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Welcome to the Everything Restaurant!");
-        System.out.print("I am your Tip Calculator for today!");
+        System.out.print("Welcome to the Useless Restaurant!");
+        System.out.println(" I am your Tip Calculator for today!");
         System.out.print("How many people do we have dining on this fine day?");
         int groupAmount = scan.nextInt();
-        System.out.println("Please enter the tip percentage for today. (0-100)");
+        System.out.print("Please enter the tip percentage for today. (0-100)");
         double tipPercent = scan.nextDouble();
         System.out.println("We have our special menu with fixed prices for meals, or you can just choose your own order.");
+        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+        System.out.println(" ");
+        System.out.println("~ Menu ~");
+        System.out.println(" ");
+        System.out.println("rocks: $2.00");
+        System.out.println("sticks: $3.00");
+        System.out.println("small dominos pizza (recommended for 52 people): $1500");
+        System.out.println("water: $10.00");
+        System.out.println("oxygen: $25.00");
+        System.out.println("cheese: $45.00");
+        System.out.println(" ");
+        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+        System.out.println(" ");
         double userInput = 0;
         double totalCost = 0;
         int userAns = 0;
         String userItem = " ";
         List<String> itemNames = new ArrayList<>();
         while (userAns != -1) {
-            System.out.println("What is the item you wish to order? Please type in dollars and cents, eg. 12.50 (-1 if you finish ordering).");
+            System.out.print("What is the item you wish to order?");
+            scan.nextLine();
             userItem = scan.nextLine();
             itemNames.add(userItem);
-            System.out.println("Please type the price in dollars and cents, eg. 12.50 (-1 if you finish ordering).");
+            System.out.print("Please type the price of the item in dollars and cents, eg. 12.50 (-1 if you finish ordering).");
             userInput = scan.nextDouble();
             totalCost += userInput;
-            System.out.println("Do you wish to continue ordering? Type 1 to continue, or -1 if not.");
+            System.out.print("Do you wish to continue ordering? Type 1 to continue, or -1 if not.");
             userAns = scan.nextInt();
         }
+        System.out.println(" ");
+        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+        System.out.println(" ");
         double totalCostRound = Math.round(totalCost * 100.0) / 100.0;
         System.out.println("Total Bill Before Tip: $" + totalCost);
         System.out.println("Tip Percentage: " + tipPercent + "%");
@@ -54,6 +71,9 @@ public class RihamTipCalculator {
         double perPersonNewCost = newBill/groupAmount;
         double perPersonNCRound = Math.round(perPersonNewCost * 100.0) / 100.0;
         System.out.println("Total Cost Per Person: $" + perPersonNCRound);
+        System.out.println("Items Ordered: " + itemNames);
+        System.out.println(" ");
+        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
         System.out.print("Please enter your credit card's budget (not ur credit card number, don't get robbed stupid): ");
         double userBudget = scan.nextDouble();
         if (userBudget < newBillRound) {
